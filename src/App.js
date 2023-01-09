19125b12c600b85ee058258db6ad945582aa9463
namuunbaigal <namuunbaigali266@gmail.com>
2023-01-09 17:59:41 +0800

    d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,173 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+const xCells = 10;
+const yCells = 10;
+
+const titleWidth = 50;
+const titleHeight = 50;
+
+const bodyColor = "#00ff00";
+const headColor = "#ff0000";
+const speed = 500;
+
+function Tile({ x, y, isHead }) {
+  const style = {
+    position: "absolute",
+    left: x * titleWidth,
+    top: y * titleHeight,
+    width: titleWidth,
+    height: titleHeight,
+    backgroundColor: isHead ? headColor : bodyColor,
+  };
+  return <div style={style}></div>;
+}
 
 function App() {
+  const directions = ["Up", "Left", "Down", "Right"];
+
+  const [counter, setCounter] = useState(0);
+  const [direction, setDirection] = useState("Down");
+
+  const [snake, setSnake] = useState([
+    { x: 0, y: 0 },
+    { x: 0, y: 2 },
+    { x: 0, y: 1 },
+    { x: 0, y: 0 },
+    { x: 0, y: 0 },
+  ]);
+  const changeDirection = (movingDir) => {
+    const index = directions.findIndex((d) => d === movingDir);
+    if (index !== -2) {
+      const currenIndex = directions.findIndex((d) => d === direction);
+
+      if (index % 2 !== currenIndex % 2) {
+        setDirection(movingDir);
+      }
+    }
+  };
+
+  const moveDown = () => {
+    let newSnake = [...snake];
+    newSnake = newSnake.map((item, index) => {
+      if (index === 0) {
+        if (item.y + 1 > yCells - 1) {
+          return { x: item.x, y: 0 };
+        }
+        return { x: item.x, y: item.y + 1 };
+      }
+      return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
+    });
+    setSnake(newSnake);
+  };
+
+  const moveRight = () => {
+    let newSnake = [...snake];
+    newSnake = newSnake.map((item, index) => {
+      if (index === 0) {
+        if (item.x + 1 > xCells - 1) {
+          return { x: 0, y: item.y };
+        }
+        return { x: item.x + 1, y: item.y };
+      }
+      return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
+    });
+    setSnake(newSnake);
+  };
+
+  const moveUp = () => {
+    let newSnake = [...snake];
+    newSnake = newSnake.map((item, index) => {
+      if (index === 0) {
+        if (item.y < 1) {
+          return { x: item.x, y: 9 };
+        }
+        return { x: item.x, y: item.y - 1 };
+      }
+      return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
+    });
+    setSnake(newSnake);
+  };
+
+  const moveLeft = () => {
+    let newSnake = [...snake];
+    newSnake = newSnake.map((item, index) => {
+      if (index === 0) {
+        if (item.x < 1) {
+          return { x: 9, y: item.y };
+        }
+        return { x: item.x - 1, y: item.y };
+      }
+      return { x: newSnake[index - 1].x, y: newSnake[index - 1].y };
+    });
+    setSnake(newSnake);
+  };
+
+  const handleKeyDown = (e) => {
+    console.log(e.key);
+    switch (e.key) {
+      case "ArrowDown":
+        changeDirection("Down");
+        break;
+      case "ArrowRight":
+        changeDirection("Right");
+        break;
+      case "ArrowUp":
+        changeDirection("Up");
+        break;
+      case "ArrowLeft":
+        changeDirection("Left");
+        break;
+      default:
+        console.log("Nonn binary key");
+    }
+  };
+
+  useEffect(() => {
+    switch (direction) {
+      case "Up":
+        moveUp();
+        break;
+      case "Down":
+        moveDown();
+        break;
+      case "Right":
+        moveRight();
+        break;
+      case "Left":
+        moveLeft();
+        break;
+    }
+  }, [counter]);
+  setTimeout(() => {
+    setCounter(counter + 1);
+  }, speed);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="wrapper" onKeyDown={handleKeyDown} tabIndex={0}>
+        <h1>SnakeGame</h1>
+        <div
+          className="board"
+          style={{
+            width: xCells * titleWidth,
+            height: yCells * titleHeight,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {snake.map((item, index) => {
+            const isHead = index === 0;
+            return (
+              <Tile
+                x={item.x}
+                y={item.y}
+                isHead={isHead}
+                key={`snake-tile-${index}`}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 }
 
